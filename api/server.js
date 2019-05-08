d2b6abb50f8265eb43a8aade3da40bd19f2e629d
Josh Akeman <joshakeman@gmail.com>
2019-05-08 18:37:21 -0400

    commit

--- api/server.js
+++ api/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const authRouter = require('../auth/auth-router')
+const userRouter = require('../users/users-router')
 
 const server = express()
 
 server.use(express.json())
+
+server.use('/api/auth', authRouter)
+server.use('/api/users', userRouter)
 
 server.use('/', (req, res) => {
     res.send('server is running')
